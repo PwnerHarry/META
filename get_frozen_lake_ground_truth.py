fae323a0614f991007f11708b517389020bd2753
Harry Zhao <mingdeharry@outlook.com>
2019-04-23 12:34:18 -0400

    ...
    ...

--- get_frozen_lake_ground_truth.py
+++ get_frozen_lake_ground_truth.py
@@ -3,7 +3,7 @@ from methods import *
 from joblib import Parallel, delayed
 from MC import *
 from true_online_GTD import *
-import numpy.matlib
+import numpy.matlib, os
 from frozen_lake import FrozenLakeEnv
 
 unit = 1
@@ -11,7 +11,7 @@ env = FrozenLakeEnv(None, '4x4', True, unit)
 N = env.observation_space.n
 
 runtimes = 10
-mc_episodes = int(1e7)
+mc_episodes = int(1e6)
 gamma = lambda x: 0.95
 runtime = 0
 target_policy = np.matlib.repmat(np.ones((1, env.action_space.n)) / env.action_space.n, env.observation_space.n, 1)
@@ -19,12 +19,20 @@ true_expectations = np.zeros((runtimes, env.action_space.n ** 2))
 true_variances = np.zeros((runtimes, env.action_space.n ** 2))
 stationary_dists = np.zeros((runtimes, env.action_space.n ** 2))
 
-while runtime < runtimes:
-    filename = 'frozenlake/frozen_lake_ground_truths_uniform_%d_%d.npz' % (mc_episodes, runtime)
-    loaded = np.load(filename)
-    true_expectations[runtime, :] = loaded['true_expectation']
-    true_variances[runtime, :] = loaded['true_variance']
-    stationary_dists[runtime, :] = loaded['stationary_dist']
-    runtime += 1
+cumulative_expectation = np.zeros((1, env.action_space.n ** 2))
+cumulative_variance = np.zeros((1, env.action_space.n ** 2))
+cumulative_distribution = np.zeros((1, env.action_space.n ** 2))
+count = 0
+
+directory = 'frozenlake'
+filelist = os.listdir(directory)
+for filename in filelist:
+    if filename.find('frozenlake_truths_uniform_%d' % mc_episodes) == -1: continue
+    loaded = np.load(directory + '/' + filename)
+    cumulative_expectation += loaded['true_expectation'].reshape(1, -1)
+    cumulative_variance += loaded['true_variance'].reshape(1, -1)
+    cumulative_distribution += loaded['stationary_dist'].reshape(1, -1)
+    count += 1
+
 filename = 'frozenlake_truths_uniform_4x4.npz'
-np.savez(filename, true_expectation = np.mean(true_expectations, axis=0), true_variance = np.mean(true_variances, axis=0), stationary_dist = np.mean(stationary_dists, axis=0))
+np.savez(filename, true_expectation = cumulative_expectation / count, true_variance = cumulative_variance / count, stationary_dist = cumulative_distribution / count)
--- get_truth_MC.py
+++ get_truth_MC.py
@@ -4,7 +4,7 @@ from joblib import Parallel, delayed
 from MC import *
 import numpy.matlib
 from frozen_lake import FrozenLakeEnv
-import random
+import random, multiprocessing
 
 def rollout_MC_per_run(env, runtime, runtimes, episodes, target, gamma, Lambda, alpha, beta):
     print('rolling out %d of %d for MC' % (runtime + 1, runtimes))
@@ -14,10 +14,13 @@ def rollout_MC_per_run(env, runtime, runtimes, episodes, target, gamma, Lambda, 
 
 def rollout_MC(env, target, gamma = lambda x: 0.95, runtimes=20, episodes=100000):
     results = Parallel(n_jobs = -1)(delayed(rollout_MC_per_run)(env, runtime, runtimes, episodes, target, gamma, None, None, None) for runtime in range(runtimes))
-    expectation_returns = [entry[0] for entry in results]
-    variance_returns = [entry[1] for entry in results]
-    state_dists = [entry[2] for entry in results]
-    return np.concatenate(expectation_returns, axis=0), np.concatenate(variance_returns, axis=0), np.concatenate(state_dists, axis=0)
+    expectation_returns = [entry[0].reshape(1, -1) for entry in results]
+    variance_returns = [entry[1].reshape(1, -1) for entry in results]
+    state_dists = [entry[2].reshape(1, -1) for entry in results]
+    expectations = np.concatenate(expectation_returns, axis=0)
+    variances = np.concatenate(variance_returns, axis=0)
+    distributions = np.concatenate(state_dists, axis=0)
+    return np.mean(expectations, axis = 0), np.mean(variances, axis = 0), np.mean(distributions, axis = 0)
 
 unit = 1
 env = FrozenLakeEnv(None, '4x4', True, unit)
@@ -28,6 +31,6 @@ gamma = lambda x: 0.95
 target_policy = np.matlib.repmat(np.array([0.2, 0.3, 0.3, 0.2]).reshape(1, 4), env.observation_space.n, 1)
 
 while True:
-    filename = 'frozenlake_truths_uniform_%d_%d.npz' % (episodes, random.randint(0, 1e6))
-    true_expectation, true_variance, stationary_dist = rollout_MC(env, target_policy, gamma = gamma, runtimes=40, episodes=episodes)
-    np.savez(filename, true_expectation = true_expectation, true_variance = true_variance, stationary_dist = stationary_dist)
+    filename = 'frozenlake' + '/' + 'frozenlake_truths_uniform_%d_%d.npz' % (episodes, random.randint(0, 1e6))
+    true_expectation, true_variance, stationary_dist = rollout_MC(env, target_policy, gamma = gamma, runtimes=multiprocessing.cpu_count(), episodes=episodes)
+    np.savez(filename, true_expectation = true_expectation.reshape, true_variance = true_variance, stationary_dist = stationary_dist)
