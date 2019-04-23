3ae899119c0fb05e56b32d4db44e36ef2655b1dc
Harry Zhao <mingdeharry@outlook.com>
2019-04-23 00:36:26 -0400

    Update frozenlake_MTA.py

--- frozenlake_MTA.py
+++ frozenlake_MTA.py
@@ -41,27 +41,24 @@ true_expectation = true_expectation * unit
 true_variance = true_variance * (unit ** 2)
 
 N = args.N ** 2
-
+things_to_save = {}
 if kappa == 0.1:
     BASELINE_LAMBDAS = [0, 0.2, 0.4, 0.6, 0.8, 1]
-    things_to_save = {}
     for baseline_lambda in BASELINE_LAMBDAS:
         Lambda = LAMBDA(env, lambda_type = 'constant', initial_value = baseline_lambda * np.ones(N))
         results = eval_method(true_online_gtd, env, true_expectation, stationary_dist, behavior_policy, target_policy, Lambda, gamma = gamma, alpha=alpha, beta=beta, runtimes=runtimes, episodes=episodes)
         exec("things_to_save[\'togtd_%g_results\'] = results.copy()" % (baseline_lambda * 1e5))
+    _, error_var_greedy, direct_greedy_results, lambda_trace_greedy = eval_greedy(env, true_expectation, true_variance, stationary_dist, behavior_policy, target_policy, gamma = gamma, alpha=alpha, beta=beta, runtimes=runtimes, episodes=episodes)
+    things_to_save['lambda_trace_greedy'] = lambda_trace_greedy
+    things_to_save['direct_greedy_results'] = direct_greedy_results
 
 _, error_L_exp, error_L_var, error_value, lambda_trace_mta = eval_MTA(env, true_expectation, true_variance, stationary_dist, behavior_policy, target_policy, kappa = kappa, gamma = gamma, alpha=alpha, beta=beta, runtimes=runtimes, episodes=episodes)
-_, error_var_greedy, direct_greedy_results, lambda_trace_greedy = eval_greedy(env, true_expectation, true_variance, stationary_dist, behavior_policy, target_policy, gamma = gamma, alpha=alpha, beta=beta, runtimes=runtimes, episodes=episodes)
- 
-things_to_save = {}
 
 things_to_save['error_value'] = error_value
 things_to_save['error_L_exp'] = error_L_exp
 things_to_save['error_L_var'] = error_L_var
 things_to_save['error_var_greedy'] = error_var_greedy
 things_to_save['lambda_trace_mta'] = lambda_trace_mta
-things_to_save['lambda_trace_greedy'] = lambda_trace_greedy
-things_to_save['direct_greedy_results'] = direct_greedy_results
 
 filename = 'frozenlake_N_%s_behavior_%g_target_%g_episodes_%g_kappa_%g' % (N, behavior_policy[0, 0], target_policy[0, 0], episodes, kappa)
 scipy.io.savemat(filename, things_to_save)
