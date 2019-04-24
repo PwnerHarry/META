69b6b6f12f4e60a518114e4d24b8425da694e77b
Harry Zhao <mingdeharry@outlook.com>
2019-04-23 20:49:00 -0400

    ...
    ...

--- gadgets/ringworld_MTA_value.m
+++ gadgets/ringworld_MTA_value.m
@@ -1,11 +1,11 @@
-expectation_list = ["off_togtd_00_results", ...
-                    "off_togtd_02_results", ...
-                    "off_togtd_04_results", ...
-                    "off_togtd_06_results", ...
-                    "off_togtd_08_results", ...
-                    "off_togtd_10_results", ...
-                    "direct_greedy_results", ...
-                    "error_value"];
+expectation_list = ["error_value_togtd_0", ...
+    "error_value_togtd_20000", ...
+    "error_value_togtd_40000", ...
+    "error_value_togtd_60000", ...
+    "error_value_togtd_80000", ...
+    "error_value_togtd_100000", ...
+    "error_value_greedy", ...
+    "error_value_mta"];
 LineColors = [linspecer(numel(expectation_list) - 2); [1, 0, 0]; [0, 0, 1];];
 num_points = 201;
 
@@ -16,26 +16,31 @@ CURVES = []; LEGENDS = {};
 figure();
 
 for result_index = 1: numel(expectation_list)
-   result_name  = expectation_list(result_index);
+    result_name  = expectation_list(result_index);
+    try
+        eval(sprintf('results = %s;', result_name))
+    catch ME
+        continue;
+    end
     eval(sprintf('results = %s;', result_name))
     [X, MEAN, INTERVAL] = get_statistics(results, num_points);
     [CURVE, ~] = band_drawer(X, MEAN, INTERVAL, LineColors(result_index, :)); %X, MEAN, INTERVAL, COLOR
     CURVES = [CURVES, CURVE];
-    if strcmp(result_name, "error_value")
+    if strcmp(result_name, "error_value_mta")
         LEGEND = "MTA";
-    elseif strcmp(result_name, "off_togtd_00_results")
+    elseif strcmp(result_name, "error_value_togtd_0")
         LEGEND = "GTD(0)";
-    elseif strcmp(result_name, "off_togtd_02_results")
+    elseif strcmp(result_name, "error_value_togtd_20000")
         LEGEND = "GTD(0.2)";
-    elseif strcmp(result_name, "off_togtd_04_results")
+    elseif strcmp(result_name, "error_value_togtd_40000")
         LEGEND = "GTD(0.4)";
-    elseif strcmp(result_name, "off_togtd_06_results")
+    elseif strcmp(result_name, "error_value_togtd_60000")
         LEGEND = "GTD(0.6)";
-    elseif strcmp(result_name, "off_togtd_08_results")
+    elseif strcmp(result_name, "error_value_togtd_80000")
         LEGEND = "GTD(0.8)";
-    elseif strcmp(result_name, "off_togtd_10_results")
+    elseif strcmp(result_name, "error_value_togtd_100000")
         LEGEND = "GTD(1)";
-    elseif strcmp(result_name, "direct_greedy_results")
+    elseif strcmp(result_name, "error_value_greedy")
         LEGEND = "greedy";
     end
     LEGENDS = [LEGENDS, LEGEND];
