1c15de9ab8a5820d2988fb6e7cecf702f67a5e36
Harry Zhao <mingdeharry@outlook.com>
2019-06-26 22:19:02 -0400

    Update ringworld.sh

--- ringworld.sh
+++ ringworld.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-bengioy
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=40
 #SBATCH --mem=64G
-#SBATCH --time=24:0:0
+#SBATCH --time=12:0:0
 
 module load python/3.7 scipy-stack
 source ~/ENV/bin/activate
