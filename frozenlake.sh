610868a60a0656d7f65a84d0d18f99389a558e58
Harry Zhao <mingdeharry@outlook.com>
2019-07-01 16:53:40 -0400

    Update frozenlake.sh

--- frozenlake.sh
+++ frozenlake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-bengioy
 #SBATCH --cpus-per-task=40
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --time=12:00:00
 
 module load python/3.7 scipy-stack
 source ~/ENV/bin/activate
 
-python evaluate_frozenlake.py --off_policy 1 --episodes 1000000 --runtimes 40 --alpha 0.05 --beta 0.05 --kappa 0.01
-python evaluate_frozenlake.py --off_policy 0 --episodes 1000000 --runtimes 40 --alpha 0.05 --beta 0.05 --kappa 0.01
+python evaluate_frozenlake.py --off_policy 1 --episodes 1000000 --runtimes 40 --alpha 0.1 --beta 0.1 --kappa 0.1
+python evaluate_frozenlake.py --off_policy 0 --episodes 1000000 --runtimes 40 --alpha 0.1 --beta 0.1 --kappa 0.1
