4249be1cfe7ff2b800c0bb22204f8d45ab7e7891
Harry Zhao <mingdeharry@outlook.com>
2019-07-01 16:52:33 -0400

    Update frozenlake.sh

--- frozenlake.sh
+++ frozenlake.sh
@@ -7,5 +7,5 @@
 module load python/3.7 scipy-stack
 source ~/ENV/bin/activate
 
-python evaluate_frozenlake.py --off_policy 1 --episodes 1000000 --runtimes 40 --learner_type togtd --alpha 0.05 --beta 0.05 --kappa 0.025
-python evaluate_frozenlake.py --off_policy 0 --episodes 1000000 --runtimes 40 --learner_type togtd --alpha 0.05 --beta 0.05 --kappa 0.025
+python evaluate_frozenlake.py --off_policy 1 --episodes 1000000 --runtimes 40 --alpha 0.05 --beta 0.05 --kappa 0.01
+python evaluate_frozenlake.py --off_policy 0 --episodes 1000000 --runtimes 40 --alpha 0.05 --beta 0.05 --kappa 0.01
