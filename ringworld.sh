1139f95bac516e50a1354c90870f28ec3b3064be
Harry Zhao <mingdeharry@outlook.com>
2019-07-03 00:24:03 -0400

    Update ringworld.sh

--- ringworld.sh
+++ ringworld.sh
@@ -6,7 +6,7 @@
 
 module load python/3.7 scipy-stack
 source ~/ENV/bin/activate
-python evaluate_ringworld.py --alpha 0.1 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.05 --target 0.05
-python evaluate_ringworld.py --alpha 0.1 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.15 --target 0.05
-python evaluate_ringworld.py --alpha 0.1 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.25 --target 0.25
-python evaluate_ringworld.py --alpha 0.1 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.33 --target 0.25
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.05 --target 0.05
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.15 --target 0.05
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.25 --target 0.25
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.33 --target 0.25
