591336b708ff7f8e8fe1d4a0e83239e23a43d007
Harry Zhao <mingdeharry@outlook.com>
2019-07-02 22:11:37 -0400

    ...

--- ringworld.sh
+++ ringworld.sh
@@ -2,8 +2,13 @@
 #SBATCH --account=def-bengioy
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=64G
-#SBATCH --time=12:0:0
+#SBATCH --time=24:0:0
 
 module load python/3.7 scipy-stack
 source ~/ENV/bin/activate
-python evaluate_ringworld.py --episodes 1000000 --runtimes 40 --behavior 0.5 --target 0.4 --learner_type totd --alpha 0.005 --beta 0.005 --kappa 0.001
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.05 --target 0.05 --learner_type totd 
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.15 --target 0.05 --learner_type totd
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.25 --target 0.25 --learner_type totd
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.33 --target 0.25 --learner_type totd
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.4 --target 0.4 --learner_type totd
+python evaluate_ringworld.py --alpha 0.001 --kappa 0.01 --episodes 1000000 --runtimes 40 --behavior 0.5 --target 0.4 --learner_type totd
--- utils.py
+++ utils.py
@@ -1,7 +1,51 @@
 import gym, gym.utils.seeding, numpy as np
 from matplotlib import pyplot as plt
 from joblib import Parallel, delayed
-from VARIABLE_LAMBDA import LAMBDA
+
+class LAMBDA():# state-based parametric lambda
+    def __init__(self, env, initial_value, approximator = 'constant'):
+        self.n = env.observation_space.n
+        self.approximator = approximator
+        if approximator == 'constant':
+            self.w = initial_value
+        elif approximator == 'linear':
+            self.w = initial_value.reshape(-1)
+        elif approximator == 'tabular':
+            self.w = initial_value.reshape(-1)
+        elif approximator == 'NN':
+            pass # Neural Network approximator to be implemented using PyTorch
+
+    def value(self, x):
+        if self.approximator == 'constant':
+            l = self.w
+        elif self.approximator == 'tabular':
+            l = self.w[x]
+        elif self.approximator == 'linear':
+            l = np.dot(x.reshape(-1), self.w)
+        elif self.approximator == 'NN':
+            pass # not implemented
+        if l > 1:
+            # print('lambda value greater than 1, truncated to 1')
+            return 1
+        elif l < 0:
+            # print('lambda value less than 0, truncated to 0')
+            return 0
+        return l
+
+    def gradient(self, x):
+        if self.approximator == 'linear':
+            return x.reshape(-1)
+
+    def gradient_descent(self, x, step_length):
+        gradient = self.gradient(x)
+        value_after = np.dot(x.reshape(-1), (self.w - step_length * gradient))
+        if value_after > 1:
+            pass # overflow of lambda rejected
+        elif value_after < 0:
+            pass # underflow of lambda rejected
+        else:
+            self.w -= step_length * gradient
+        self.w -= step_length * gradient
 
 class RingWorldEnv(gym.Env):
     def __init__(self, N):
