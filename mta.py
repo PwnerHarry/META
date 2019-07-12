62a061329e26cec4bdad61e7acf9779fb8e6ff50
Harry Zhao <mingdeharry@outlook.com>
2019-07-12 12:17:09 -0400

    Update mta.py

--- mta.py
+++ mta.py
@@ -20,15 +20,15 @@ def MTA(env, episodes, target, behavior, evaluate, Lambda, encoder, learner_type
             action = decide(o_curr, behavior)
             rho_curr = importance_sampling_ratio(target, behavior, o_curr, action)
             log_rho_accu += np.log(target[o_curr, action]) - np.log(behavior[o_curr, action])
-            o_next, R_next, done, _ = env.step(action)
+            o_next, r_next, done, _ = env.step(action)
             x_next = encoder(o_next)
             if learner_type == 'togtd':
-                MC_exp_learner.learn(R_next, gamma(x_next), gamma(x_curr), x_next, x_curr, 1.0, 1.0, rho_curr, alpha, beta)
-                L_exp_learner.learn(R_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, min(1.0, 1.1 * alpha), min(1.0, 1.1 * beta))
+                MC_exp_learner.learn(r_next, gamma(x_next), gamma(x_curr), x_next, x_curr, 1.0, 1.0, rho_curr, alpha, beta)
+                L_exp_learner.learn(r_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, min(1.0, 1.1 * alpha), min(1.0, 1.1 * beta))
             elif learner_type == 'totd':
-                MC_exp_learner.learn(R_next, gamma(x_next), gamma(x_curr), x_next, x_curr, 1.0, 1.0, rho_curr, alpha)
-                L_exp_learner.learn(R_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, min(1.0, 1.1 * alpha))
-            delta_curr = R_next + float(not done) * gamma(x_next) * np.dot(x_next, value_learner.w_curr) - np.dot(x_curr, value_learner.w_curr)
+                MC_exp_learner.learn(r_next, gamma(x_next), gamma(x_curr), x_next, x_curr, 1.0, 1.0, rho_curr, alpha)
+                L_exp_learner.learn(r_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, min(1.0, 1.1 * alpha))
+            delta_curr = r_next + float(not done) * gamma(x_next) * np.dot(x_next, value_learner.w_curr) - np.dot(x_curr, value_learner.w_curr)
             r_bar_next = delta_curr ** 2
             gamma_bar_next = (Lambda.value(x_next) * gamma(x_next)) ** 2
             if learner_type == 'togtd':
@@ -36,16 +36,15 @@ def MTA(env, episodes, target, behavior, evaluate, Lambda, encoder, learner_type
             elif learner_type == 'totd':
                 L_var_learner.learn(r_bar_next, gamma_bar_next, 1, x_next, x_curr, 1, 1, rho_curr, alpha)
             # SGD on meta-objective
-            rho_acc = np.exp(log_rho_accu)
             v_next = np.dot(x_next, value_learner.w_curr)
-            var_L_next, exp_L_next, exp_MC_next = np.dot(x_next, L_var_learner.w_curr), np.dot(x_next, L_exp_learner.w_curr), np.dot(x_next, MC_exp_learner.w_curr)
-            coefficient = gamma(x_next) ** 2 * Lambda.value(x_next) * ((v_next - exp_L_next) ** 2 + var_L_next) + v_next * (exp_L_next + exp_MC_next) - v_next ** 2 - exp_L_next * exp_MC_next
-            Lambda.GD(x_next, kappa * rho_acc * coefficient)
+            VmE = v_next - np.dot(x_next, L_exp_learner.w_curr)
+            coefficient = gamma(x_next) ** 2 * (Lambda.value(x_next) * (VmE ** 2 + np.dot(x_next, L_var_learner.w_curr)) + VmE * (v_next - np.dot(x_next, MC_exp_learner.w_curr)))
+            Lambda.GD(x_next, kappa * np.exp(log_rho_accu) * coefficient)
             # learn value
             if learner_type == 'togtd':
-                value_learner.learn(R_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, alpha, beta)
+                value_learner.learn(r_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, alpha, beta)
             elif learner_type == 'totd':
-                value_learner.learn(R_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, alpha)
+                value_learner.learn(r_next, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), Lambda.value(x_curr), rho_curr, alpha)
             MC_exp_learner.next(); L_exp_learner.next(); L_var_learner.next(); value_learner.next()
             o_curr, x_curr = o_next, x_next
     return value_trace
