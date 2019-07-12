a8b379e02b28f8123d6320ad2b036db5b9c3399b
Harry Zhao <mingdeharry@outlook.com>
2019-07-12 12:50:10 -0400

    ...

--- ringworld.sh
+++ ringworld.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=24:0:0
 
 ALPHA="1"
-TARGET="0.05"
-BEHAVIOR="0.1"
+TARGET="0.35"
+BEHAVIOR="0.4"
 RUNTIMES="240"
 EPISODES="100000"
 
@@ -15,6 +15,8 @@ source ~/ENV/bin/activate
 python -m compileall ./
 
 python evaluate_ringworld.py --alpha $ALPHA --kappa 0 --episodes $EPISODES --runtimes $RUNTIMES --behavior $BEHAVIOR --target $TARGET --evaluate_MTA 0
+
+python evaluate_ringworld.py --alpha $ALPHA --kappa 0.00001 --episodes $EPISODES --runtimes $RUNTIMES --behavior $BEHAVIOR --target $TARGET --evaluate_baselines 0 --evaluate_greedy 0
 python evaluate_ringworld.py --alpha $ALPHA --kappa 0.0001 --episodes $EPISODES --runtimes $RUNTIMES --behavior $BEHAVIOR --target $TARGET --evaluate_baselines 0 --evaluate_greedy 0
 python evaluate_ringworld.py --alpha $ALPHA --kappa 0.001 --episodes $EPISODES --runtimes $RUNTIMES --behavior $BEHAVIOR --target $TARGET --evaluate_baselines 0 --evaluate_greedy 0
 python evaluate_ringworld.py --alpha $ALPHA --kappa 0.01 --episodes $EPISODES --runtimes $RUNTIMES --behavior $BEHAVIOR --target $TARGET --evaluate_baselines 0 --evaluate_greedy 0
