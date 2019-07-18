456e6521d18b2b8216568aa2d133cc8e3fbae99f
Harry Zhao <mingdeharry@outlook.com>
2019-07-18 15:07:58 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -102,7 +102,7 @@ class RingWorldEnv(gym.Env):
 # EVALUATION METHODS
 @jit(nopython=True, cache=True)
 def mse(estimate, target, weight):
-    return np.linalg.norm(np.multiply(target - estimate, weight), 2) ** 2
+    return np.linalg.norm(np.multiply(estimate - target, weight), 2) ** 2
 
 def evaluate_estimate(weight, expectation, variance, distribution, stat_type, state_set_matrix):
     # place the state representations row by row in the state_set_matrix
