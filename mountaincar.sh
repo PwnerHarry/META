7bed476bbe48599f60a0846c48a6a8f74a67ba50
Harry Zhao <mingdeharry@outlook.com>
2019-08-03 16:41:00 -0400

    ...

--- mountaincar.sh
+++ mountaincar.sh
@@ -7,10 +7,16 @@
 # DEFAULT VALUES
 RUNTIMES="240"
 EPISODES="10000"
+ETA="0.1"
+ALPHA="0.1"
 
 # PARSE ARGS
 while [ "$1" != "" ]; do
     case $1 in
+        -a | --alpha )          
+                                shift
+                                ALPHA=$1
+                                ;;
         --episodes )       
                                 shift
                                 EPISODES=$1
@@ -25,25 +31,22 @@ done
 
 echo "runtimes: $RUNTIMES, episodes: $EPISODES"
 
-for ALPHA in 0.01 0.02 0.03 0.04 0.05 0.001 0.002 0.003 0.004 0.005 0.0001 0.0002 0.0003 0.0004 0.0005
-do
-    python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_MTA 0 --alpha $ALPHA
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_MTA 0 --alpha $ALPHA --eta $ETA
 
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 1 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 2 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 3 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 4 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 5 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 1 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 2 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 3 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 4 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 5 * $ALPHA}"`
 
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.1 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.2 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.3 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.4 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.5 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.1 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.2 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.3 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.4 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.5 * $ALPHA}"`
 
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.01 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.02 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.03 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.04 * $ALPHA}"`
-    # python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --kappa `awk "BEGIN {print 0.05 * $ALPHA}"`
-done
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.01 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.02 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.03 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.04 * $ALPHA}"`
+python control_mountaincar.py --runtimes $RUNTIMES --episodes $EPISODES --evaluate_baselines 0 --evaluate_greedy 0 --alpha $ALPHA --eta $ETA --kappa `awk "BEGIN {print 0.05 * $ALPHA}"`
