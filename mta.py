b617e2f8f9a154c0bcffe085587e4e7cb3f092b7
Harry Zhao <mingdeharry@outlook.com>
2019-08-06 19:01:16 -0400

    Update mta.py

--- mta.py
+++ mta.py
@@ -5,6 +5,10 @@ from TOTD import *
 
 def MTA(env, episodes, target, behavior, evaluate, Lambda, encoder, learner_type='togtd', gamma=lambda x: 0.95, alpha=0.05, beta=0.05, kappa=0.01):
     D = np.size(encoder(env.reset()))
+    if np.size(Lambda.w) == D:
+        encoder_lambda = encoder
+    else:
+        encoder_lambda = lambda x: onehot(x, env.observation_space.n)
     value_trace = np.empty(episodes); value_trace[:] = np.nan
     if learner_type == 'togtd':
         LEARNER, slow_lr_dict, fast_lr_dict = TOGTD_LEARNER, {'alpha_curr': alpha, 'beta_curr': beta}, {'alpha_curr': min(1.0, 2 * alpha), 'beta_curr': min(1.0, 2 * alpha)}
@@ -24,7 +28,7 @@ def MTA(env, episodes, target, behavior, evaluate, Lambda, encoder, learner_type
                 log_rho_accu += np.log(rho_curr)
                 o_next, r_next, done, _ = env.step(action)
                 x_next = encoder(o_next)
-                lambda_curr, lambda_next = Lambda.value(x_curr), Lambda.value(x_next)
+                lambda_curr, lambda_next = Lambda.value(encoder_lambda(o_curr)), Lambda.value(encoder_lambda(o_next))
                 MC_exp_learner.learn(r_next, done, gamma(x_next), gamma(x_curr), x_next, x_curr, 1, 1, rho_curr, **fast_lr_dict)
                 L_exp_learner.learn(r_next, done, gamma(x_next), gamma(x_curr), x_next, x_curr, lambda_next, lambda_curr, rho_curr, **fast_lr_dict)
                 v_next = float(not done) * np.dot(x_next, value_learner.w_curr)
@@ -35,9 +39,10 @@ def MTA(env, episodes, target, behavior, evaluate, Lambda, encoder, learner_type
                 L_var_next = np.dot(x_next, L_var_learner.w_curr)
                 if L_var_next > np.sqrt(np.finfo(float).eps):
                     coefficient = gamma(x_next) ** 2 * (lambda_next * (VmE ** 2 + L_var_next) + VmE * (v_next - np.dot(x_next, MC_exp_learner.w_curr)))                        
-                    Lambda.GD(x_next, kappa * np.exp(log_rho_accu) * coefficient)
+                    Lambda.GD(encoder_lambda(o_next), kappa * np.exp(log_rho_accu) * coefficient)
+                    lambda_next = Lambda.value(encoder_lambda(o_next))
                 # learn value
-                value_learner.learn(r_next, done, gamma(x_next), gamma(x_curr), x_next, x_curr, Lambda.value(x_next), lambda_curr, rho_curr, **slow_lr_dict)
+                value_learner.learn(r_next, done, gamma(x_next), gamma(x_curr), x_next, x_curr, lambda_next, lambda_curr, rho_curr, **slow_lr_dict)
                 for learner in learners: learner.next()
                 o_curr, x_curr = o_next, x_next
         except RuntimeWarning:
@@ -63,12 +68,9 @@ def eval_MTA(env_name, behavior, target, kappa, gamma, alpha, beta, runtimes, ep
         # initial_weights_lambda = np.linalg.lstsq(get_state_set_matrix(env, encoder), np.ones(env.observation_space.n), rcond=None)[0]
         initial_weights_lambda = np.zeros(np.size(encoder(0)))
     else:
-        initial_weights_lambda = np.ones(env.observation_space.n)
+        initial_weights_lambda = np.zeros(np.size(onehot(0, env.observation_space.n)))
     LAMBDAS = []
     for runtime in range(runtimes):
-        if parametric_lambda:
-            LAMBDAS.append(LAMBDA(env, initial_value=initial_weights_lambda, approximator='linear'))
-        else:
-            LAMBDAS.append(LAMBDA(env, initial_value=initial_weights_lambda, approximator='tabular', state_set_matrix=get_state_set_matrix(env, encoder)))
+        LAMBDAS.append(LAMBDA(env, initial_value=initial_weights_lambda, approximator='linear'))
     results = Parallel(n_jobs=-1)(delayed(eval_MTA_per_run)(env_name, runtime, runtimes, episodes, target, behavior, kappa, gamma, LAMBDAS[runtime], alpha, beta, evaluate, encoder, learner_type) for runtime in range(runtimes))
     return np.concatenate(results, axis=0)
